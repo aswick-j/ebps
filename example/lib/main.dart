d8e9063e9f491ac21fe3624840ace6aa8b36dfcd
a_aswickj <a_aswickj@equitasbank.com>
2024-04-15 17:27:12 +0530

    Test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Demos',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
