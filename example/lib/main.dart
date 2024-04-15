be05152ff3217dc5cf0793edd76a42e2edf14e5b
a_aswickj <a_aswickj@equitasbank.com>
2024-04-15 17:51:29 +0530

    Replace

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demos',
+      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
