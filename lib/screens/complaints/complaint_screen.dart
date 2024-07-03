6a9276acad5f465bcd43aa477da90d8067ca677d
a_aswickj <a_aswickj@equitasbank.com>
2024-07-03 13:26:35 +0530

    VERSION

--- lib/screens/complaints/complaint_screen.dart
+++ lib/screens/complaints/complaint_screen.dart
@@ -78,8 +78,8 @@ class _ComplaintScreenState extends State<ComplaintScreen> {
             showDuration: Duration(milliseconds: 20000),
             margin: EdgeInsets.symmetric(horizontal: 10.w),
             message: ApiConstants.BASE_URL.contains("digiservices.")
-                ? "V 1.1.4"
-                : "UAT - V 0.1.4",
+                ? "V 1.1.5"
+                : "UAT - V 0.1.5",
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Icon(
